3e5d6bf220f83ec89b3df6e7adf7c2b25e05be6f
samsul <csamsul2017@gmail.com>
2026-01-28 05:46:57 +0700

    refactor: improve user identity card structure and logic

--- src/components/signup/UserIdentityCard.jsx
+++ src/components/signup/UserIdentityCard.jsx
@@ -1,26 +1,49 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-const UserIdentityCard = () => {
+// Component Helper
+const FullnameInput = ({ title, placeholder, state, name }) => {
   return (
-    <div className="mt-16 flex justify-center">
-      <div className="text-white px-4 flex flex-col gap-4 md:border md:border-muted/30 md:rounded-3xl md:max-w-md md:py-8 md:px-8">
+    <div>
+      <p>{title}</p>
+      <input
+        type="text"
+        className="w-full p-4 bg-transparent border border-muted rounded-2xl"
+        onChange={e => {
+          const val = e.target.value;
+          if (val === '' || /^[a-zA-Z\s]+$/.test(val)) state(e.target.value);
+        }}
+        placeholder={placeholder}
+        value={name}
+        required
+      />
+    </div>
+  );
+};
+
+const UserIdentityCard = ({ firstName, setFirstName, lastName, setLastName }) => {
+  const isValidRule = firstName && lastName;
+
+  return (
+    <div className="flex justify-center mt-16">
+      <div className="flex flex-col gap-4 px-4 text-white md:border md:border-muted/30 md:rounded-3xl md:max-w-md md:py-8 md:px-8">
         <div>
           <h1 className="text-2xl font-bold">What should we call you?</h1>
           <p className="text-muted">Enter your first and last name. You can make changes later on by verifying your account.</p>
         </div>
 
-        <div className="flex flex-col gap-4">
-          <div className="">
-            <p>First name</p>
-            <input type="text" className="w-full bg-transparent border border-muted p-4 rounded-2xl" placeholder="Satoshi" />
-          </div>
-          <div>
-            <p>Last name</p>
-            <input type="text" className="w-full bg-transparent border border-muted p-4 rounded-2xl" placeholder="Nakamoto" />
-          </div>
-        </div>
-
-        <button className="bg-blue-400/60 text-black font-bold py-4 rounded-full mt-16">Continue</button>
+        <form
+          onSubmit={e => {
+            e.preventDefault();
+            alert('success');
+          }}
+          className="flex flex-col gap-4"
+        >
+          <FullnameInput title={'Firstname'} placeholder={'Satoshi'} state={setFirstName} name={firstName} />
+          <FullnameInput title={'Lastname'} placeholder={'Nakamoto'} state={setLastName} name={lastName} />
+          <button type="button" className="py-4 mt-16 font-bold text-black rounded-full bg-primary disabled:bg-blue-400/60 active:scale-95 hover:bg-primary/90" disabled={!isValidRule}>
+            Continue
+          </button>
+        </form>
 
         <p className="text-xs text-muted">
           We use strictly necessary cookies to enable essential functions, such as security and authentication. For more information, see our <span>Cookie Policy</span> and <span>Privacy policy</span>
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -8,10 +8,12 @@ import UserIdentityCard from '../components/signup/UserIdentityCard';
 import ForgetPassCard from '../components/signup/ForgetPassCard';
 
 const SignUp = () => {
-  const [step, setStep] = useState(4);
+  const [step, setStep] = useState(5);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [phoneNumber, setPhoneNumber] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
 
   return (
     <div className="bg-[#0A0B0D] py-4 min-h-screen">
@@ -20,8 +22,7 @@ const SignUp = () => {
       {step === 2 && <VerifyEmailCard email={email} onBack={() => setStep(1)} onNext={() => setStep(3)} />}
       {step === 3 && <CreatePasswordCard password={password} setPassword={setPassword} />}
       {step === 4 && <VerifyPhoneNumberCard phoneNumber={phoneNumber} setPhoneNumber={setPhoneNumber} />}
-
-      {/* <UserIdentityCard /> */}
+      {step === 5 && <UserIdentityCard firstName={firstName} setFirstName={setFirstName} lastName={lastName} setLastName={setLastName} />}
       {/* <ForgetPassCard /> */}
     </div>
   );
